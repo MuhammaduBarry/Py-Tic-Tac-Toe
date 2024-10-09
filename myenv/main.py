8ff12e7b3fc119c17f36b58f3e7a1f4396124fcd
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-10-09 11:13:37 -0400

    Created player tracking function

--- myenv/main.py
+++ myenv/main.py
@@ -2,13 +2,19 @@
 import pyfiglet
 from termcolor import colored
 
-simple_tic_tac_toe_board = f'''
-     1 | 2 | 3
+instruction_board = f'''
+     0 | 1 | 2
     ---|---|---
-     4 | 5 | 6 
+     3 | 4 | 5 
     ---|---|---
-     7 | 8 | 9  
+     6 | 7 | 8  
     '''
+
+tic_tac_toe_board = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+] 
 
 def intro():
     """Define Rules and Information needed to play"""
@@ -19,11 +25,11 @@ def intro():
     # Instructions
     intro_statement = f'''
     The rules are simple:
-    {simple_tic_tac_toe_board}
+    {instruction_board}
     This is a turn based program, The first player that will start is the first player who inputs their name and symbol.
 
-    1. After players and symbols have been selected, player 1 will choose a number between 1-9.
-    2. Then player 2 will choose another number 1-9, until the players get three in a row, column, and diagonal.
+    1. After players and symbols have been selected, player 1 will choose a number between 0-8.
+    2. Then player 2 will choose another number 0-8, until the players get three in a row, column, and diagonal.
     3. You will be prompted on how many games you would like to play, 1 up to infinity amount of games.
     4. Make sure you double check if the space number has been taken before inputting your number.
     5. Good luck and have fun :)
@@ -33,9 +39,26 @@ def intro():
     print(f"\n{intro_ascii_art}")
     print(intro_statement)
 
+def display_current_board(input, player_symbol):
+    """Function to display and track player game"""
+    
+    # Placing them accordingly
+    if input <= 2:
+        tic_tac_toe_board[0][input] = player_symbol
+    elif 2 < input <= 5:
+        # Removing index error
+        tic_tac_toe_board[1][input - 3] = player_symbol
+    elif 5 < input <= 8:
+        # Removing index error
+        tic_tac_toe_board[2][input - 6] = player_symbol
+    else:
+        print("Index out of bound wont work")
+
+    print(tic_tac_toe_board)
+
 def game_logic():
     """Create logic that will house the core program functionality"""
-    
+
     # Game information
     game_rounds = input("\nHow many rounds would you like to play? ")
 
@@ -52,3 +75,4 @@ def main():
     game_logic()
 
 main()
+
